971370ac71874e5f8f45937e04b31bdc63c93eff
unknown <13333084654@163.com>
2020-12-24 09:49:40 +0800

    r

--- pages/home/home.vue
+++ pages/home/home.vue
@@ -1,195 +1,276 @@
 <template>
-  <view>
-    <!-- 轮播图 -->
-    <view class="swiperBox">
-      <swiper
-        class="swiper"
-        :indicator-dots="indicatorDots"
-        :autoplay="autoplay"
-        :interval="interval"
-        :duration="duration"
-      >
-        <swiper-item v-for="item in imgList" :key="item.id" class="swiperImg"
-          ><image :src="item.url"></image
-        ></swiper-item>
-      </swiper>
-    </view>
-    <!-- 新闻 -->
-    <view class="news">
-      <view class="title">最近新闻</view>
-      <view class="news-item">
-        <view
-          class="padding-xl radius shadow shadow-lg bg-white margin-top news-fu flex justify-between"
-          v-for="item in dlist"
-          :key="item._id"
-          @click="opennews"
-          :data-postid="item.post_id"
-        >
-          <image :src="item.cover" mode=""></image>
-          <view class="news-text">
-            <p class="news-title">{{ item.title }}</p>
-            <p class="news-times">{{ item.author_name }}</p>
-          </view>
-        </view>
-      </view>
-    </view>
-  </view>
+	<view>
+		<!-- 搜索 -->
+		<view class="jincehng-input-box" @click="search()">
+			<input class="jincheng-input" type="text" value="" placeholder="输入关键字" />
+			<view class="text-red cu-btn round lines-red jincheng-search">搜索</view>
+		</view>
+		<!-- 轮播图 -->
+		<view class="swiperBox">
+			<swiper class="swiper" :indicator-dots="indicatorDots" :autoplay="autoplay" :interval="interval" :duration="duration">
+				<swiper-item v-for="item in imgList" :key="item.id" class="swiperImg"><image :src="item.url"></image></swiper-item>
+			</swiper>
+		</view>
+		<!-- 分类 -->
+		<view class="title">分类</view>
+		<view class="fenlei">
+			<view class=" flex justify-between fl">
+				<view class="fl-left" @click="practice">
+					<text>练习</text>
+					<br/>
+					<text>practice</text>
+				</view>
+				<view class="fl-right" @click="examination">
+					<text>考试</text>
+					<br/>
+					<text>examination</text>
+				</view>
+			</view>
+		</view>
+		<!-- 新闻 -->
+		<view class="news">
+			<view class="title">最近新闻</view>
+			<view class="news-item">
+				<view
+					class="padding-xl radius shadow shadow-lg bg-white margin-top news-fu flex justify-between"
+					v-for="item in dlist"
+					:key="item._id"
+					@click="opennews"
+					:data-postid="item.post_id"
+				>
+					<image :src="item.cover" mode=""></image>
+					<view class="news-text">
+						<p class="news-title">{{ item.title }}</p>
+						<p class="news-times">{{ item.author_name }}</p>
+					</view>
+				</view>
+			</view>
+		</view>
+	</view>
 </template>
 
 <script>
 export default {
-  data() {
-    return {
-      autoplay: true,
-      interval: 2000,
-      duration: 500,
-      indicatorDots: true,
-      dlist: [],
-      imgList: [
-        {
-          id: "1",
-          url: "http://91mlearning.com/Upload/Info/root//2014_04/HTML5.jpg",
-        },
-        {
-          id: "2",
-          url:
-            "https://atts.w3cschool.cn/attachments/cover/cover_css.png?t=1579664257&imageView2/1/w/150/h/84",
-        },
-        {
-          id: "3",
-          url:
-            "https://pic4.zhimg.com/v2-757212b6ecac206528be7c3e7e0ed135_1440w.jpg?source=172ae18b",
-        },
-      ],
-    };
-  },
-  onLoad() {
-    uni.request({
-      url: "https://unidemo.dcloud.net.cn/api/news",
-      method: "GET",
-      data: {},
-      success: (res) => {
-        this.dlist = res.data;
-        this.token = false;
-      },
-      fail: () => {},
-      complete: () => {},
-    });
-  },
-  methods: {
-    opennews(e) {
-      console.log(e.currentTarget.dataset.postid);
-      uni.navigateTo({
-        url: "./newsdate?postid=" + e.currentTarget.dataset.postid,
-      });
-    },
-    changeIndicatorDots(e) {
-      this.indicatorDots = !this.indicatorDots;
-    },
-    changeAutoplay(e) {
-      this.autoplay = !this.autoplay;
-    },
-    intervalChange(e) {
-      this.interval = e.target.value;
-    },
-    durationChange(e) {
-      this.duration = e.target.value;
-    },
-  },
+	data() {
+		return {
+			autoplay: true,
+			interval: 2000,
+			duration: 500,
+			indicatorDots: true,
+			dlist: [],
+			imgList: [
+				{
+					id: '1',
+					url: 'http://91mlearning.com/Upload/Info/root//2014_04/HTML5.jpg'
+				},
+				{
+					id: '2',
+					url: 'https://atts.w3cschool.cn/attachments/cover/cover_css.png?t=1579664257&imageView2/1/w/150/h/84'
+				},
+				{
+					id: '3',
+					url: 'https://pic4.zhimg.com/v2-757212b6ecac206528be7c3e7e0ed135_1440w.jpg?source=172ae18b'
+				}
+			]
+		};
+	},
+	onLoad() {
+		uni.request({
+			url: 'https://unidemo.dcloud.net.cn/api/news',
+			method: 'GET',
+			data: {},
+			success: res => {
+				this.dlist = res.data;
+				this.token = false;
+			},
+			fail: () => {},
+			complete: () => {}
+		});
+	},
+	methods: {
+		opennews(e) {
+			uni.navigateTo({
+				url: './newsdate?postid=' + e.currentTarget.dataset.postid
+			});
+		},
+		search() {
+			uni.switchTab({
+				url: '../search/search'
+			});
+		},examination() {
+			uni.switchTab({
+				url: '../examination/index'
+			});
+		},practice() {
+			uni.switchTab({
+				url: '../practice/index'
+			});
+		},
+		changeIndicatorDots(e) {
+			this.indicatorDots = !this.indicatorDots;
+		},
+		changeAutoplay(e) {
+			this.autoplay = !this.autoplay;
+		},
+		intervalChange(e) {
+			this.interval = e.target.value;
+		},
+		durationChange(e) {
+			this.duration = e.target.value;
+		}
+	}
 };
 </script>
 
 <style lang="less">
+/* 搜索 */
+.jincehng-input-box {
+	display: flex;
+	margin-top: 10px;
+	margin: 25px auto;
+	width: 95%;
+	.jincheng-input {
+		width: 85%;
+		height: 40px;
+		background: #fafafa;
+		border-bottom-left-radius: 18px;
+		border-top-left-radius: 18px;
+		border-top-right-radius: 18px;
+		border-bottom-right-radius: 18px;
+		padding-left: 20px;
+		margin-right: 25px;
+		border: 1px solid red;
+	}
+	.jincheng-search {
+		width: 80px;
+		height: 38px;
+		font-size: 18px;
+		margin-right: 10px;
+		font-weight: bolder;
+	}
+}
 /* 轮播 */
 .swiper {
-  width: 100%;
-  height: 150px;
+	width: 100%;
+	height: 150px;
 
-  .swiperImg {
-    width: 100%;
-    height: 100%;
-    image {
-      width: 100%;
-      height: 100%;
-    }
-  }
+	.swiperImg {
+		width: 100%;
+		height: 100%;
+		image {
+			width: 100%;
+			height: 100%;
+		}
+	}
 }
 .z-list {
-  border-radius: 15px;
-  background-color: rgb(255, 255, 255);
-  box-shadow: 0px 5px 25.92px 1.08px rgba(200, 200, 200, 0.22);
-  margin: 40px auto;
-  width: 95%;
-  height: 80px;
-  padding-top: 1px;
-  .z-h {
-    width: 85%;
-    height: 100%;
-    margin: 2% auto;
-  }
-  .z-left {
-    width: 50%;
-    height: 95%;
-    float: left;
-    text:nth-child(1) {
-      display: -webkit-box;
-      word-break: break-all;
-      -webkit-box-orient: vertical;
-      -webkit-line-clamp: 2;
-      overflow: hidden;
-      text-overflow: ellipsis;
-    }
-    text:nth-child(2) {
-      display: block;
-      font-size: 10px;
-      margin-top: 5px;
-      color: #cccccc;
-    }
-  }
-  .z-right {
-    width: 40%;
-    height: 85%;
-    border-radius: 15px;
-    overflow: hidden;
-    float: right;
-    image {
-      width: 100%;
-      height: 100%;
-    }
-  }
+	border-radius: 15px;
+	background-color: rgb(255, 255, 255);
+	box-shadow: 0px 5px 25.92px 1.08px rgba(200, 200, 200, 0.22);
+	margin: 40px auto;
+	width: 95%;
+	height: 80px;
+	padding-top: 1px;
+	.z-h {
+		width: 85%;
+		height: 100%;
+		margin: 2% auto;
+	}
+	.z-left {
+		width: 50%;
+		height: 95%;
+		float: left;
+		text:nth-child(1) {
+			display: -webkit-box;
+			word-break: break-all;
+			-webkit-box-orient: vertical;
+			-webkit-line-clamp: 2;
+			overflow: hidden;
+			text-overflow: ellipsis;
+		}
+		text:nth-child(2) {
+			display: block;
+			font-size: 10px;
+			margin-top: 5px;
+			color: #cccccc;
+		}
+	}
+	.z-right {
+		width: 40%;
+		height: 85%;
+		border-radius: 15px;
+		overflow: hidden;
+		float: right;
+		image {
+			width: 100%;
+			height: 100%;
+		}
+	}
+}
+/* 分类 */
+.fenlei {
+	width: 80%;
+	height: 70px;
+	margin: 20px auto;
+	font-size: 20px;
+	.fl {
+		width: 100%;
+		view{
+			border-radius: 10px;
+			width: 42%;
+			height: 70px;
+			color: #ffffff;
+			text{
+				font-style: italic;
+				display: block;
+				float: right;
+				margin-right: 10px;
+				line-height: 35px;
+			}
+		}
+		.fl-left {
+			text:nth-child(1){
+				font-size: 20px;
+			}
+			background-color: #93e4fe;
+		}
+		.fl-right{
+			background-color: #2CAEFF;
+		}
+	}
 }
 /* 新闻 */
 .title {
-  width: 80%;
-  margin: 20px auto 0 auto;
-  font-size: 20px;
-  font-family: 微软雅黑;
+	width: 80%;
+	margin: 20px auto 0 auto;
+	font-size: 20px;
+	font-family: 微软雅黑;
 }
 .news-fu {
-  width: 90%;
-  height: 80px;
-  margin: 20px auto;
-  padding: 10px;
-  background: no-repeat;
-  background-size: 100% 100%;
-  image {
-    width: 35%;
-    height: 100%;
-  }
+	width: 90%;
+	height: 80px;
+	margin: 20px auto;
+	padding: 10px;
+	background: no-repeat;
+	background-size: 100% 100%;
+	image {
+		width: 35%;
+		height: 100%;
+	}
 
-  .news-text {
-    width: 60%;
-    .news-title {
-      display: -webkit-box;
-      -webkit-box-orient: vertical;
-      -webkit-line-clamp: 2;
-      overflow: hidden;
-    }
-    .news-times {
-      line-height: 35px;
-      color: grey;
-      font-size: 1px;
-    }
-  }
+	.news-text {
+		width: 60%;
+		.news-title {
+			display: -webkit-box;
+			-webkit-box-orient: vertical;
+			-webkit-line-clamp: 2;
+			overflow: hidden;
+		}
+		.news-times {
+			line-height: 35px;
+			color: grey;
+			font-size: 1px;
+		}
+	}
 }
 </style>
--- pages/search/search.vue
+++ pages/search/search.vue
@@ -1,235 +1,239 @@
 <template>
-<<<<<<< HEAD
-	<view>
-		搜索页面啊啊啊
-=======
-	<view style="    margin: auto;
-    width: 95%;">
-		<view class="jincehng-input-box">
-			<input class="jincheng-input" type="text" value="" placeholder="输入关键字" v-model="searchcontent" />
-			<view class=" text-red cu-btn round lines-red jincheng-search" @click="search()">
-				搜索
-			</view>
-		</view>
-		<view style="    margin: auto;
-    width: 95%;">
-			<view class="cu-load load-modal" v-if="loadModal">
-				<view class="gray-text">加载中...</view>
-			</view>
-			<view class="margin" v-show="isSistory">
-				<view class="flex justify-between" style="width: 94%;">
-					<view class="text-orange" style="font-size: 20px; ">
-						搜索历史
-					</view>
-					<text class="text-gray cuIcon-deletefill" style="font-size: 25px;" @click="empty"></text>
-				</view>
-				<view class="flex flex-wrap">
-				<view class="jincehng-ti jincheng-font bg-grey round margin-lr-sm margin-top " v-for="(item,index) in historyList" :key='index'
-				 @click="history(item)">{{item}}</view>
-				</view>
-			</view>
-			<view class="jincehng-xuan" style="    margin: 20px auto;
-    width: 95%;" v-for="(item,index) in dataa" :key='item+index'>
-				<view class="jincehng-ti jincheng-font" @click="todetails(item.id)">
-					<view v-if="item.type=='radio'">[单选]</view>
-					<view v-if="item.type=='checkbox'">[多选]</view>
-					<view v-if="item.type=='judge'">[判断]</view>
-					{{item.name}}
-				</view>
-			
-			
-			</view>
-		</view>
-		
-
->>>>>>> wy
-	</view>
+  <view style="margin: auto; width: 95%">
+    <view class="jincehng-input-box">
+      <input
+        class="jincheng-input"
+        type="text"
+        value=""
+        placeholder="输入关键字"
+        v-model="searchcontent"
+      />
+      <view
+        class="text-red cu-btn round lines-red jincheng-search"
+        @click="search()"
+      >
+        搜索
+      </view>
+    </view>
+    <view style="margin: auto; width: 95%">
+      <view class="cu-load load-modal" v-if="loadModal">
+        <view class="gray-text">加载中...</view>
+      </view>
+      <view class="margin" v-show="isSistory">
+        <view class="flex justify-between" style="width: 94%">
+          <view class="text-orange" style="font-size: 20px"> 搜索历史 </view>
+          <text
+            class="text-gray cuIcon-deletefill"
+            style="font-size: 25px"
+            @click="empty"
+          ></text>
+        </view>
+        <view class="flex flex-wrap">
+          <view
+            class="jincehng-ti jincheng-font bg-grey round margin-lr-sm margin-top"
+            v-for="(item, index) in historyList"
+            :key="index"
+            @click="history(item)"
+            >{{ item }}</view
+          >
+        </view>
+      </view>
+      <view
+        class="jincehng-xuan"
+        style="margin: 20px auto; width: 95%"
+        v-for="(item, index) in dataa"
+        :key="item + index"
+      >
+        <view class="jincehng-ti jincheng-font" @click="todetails(item.id)">
+          <view v-if="item.type == 'radio'">[单选]</view>
+          <view v-if="item.type == 'checkbox'">[多选]</view>
+          <view v-if="item.type == 'judge'">[判断]</view>
+          {{ item.name }}
+        </view>
+      </view>
+    </view>
+  </view>
 </template>
 <script>
-	import {
-		baseUrl
-	} from "@/api/index.js"
-	export default {
-		data() {
-			return {
-				// 搜索历史是否显示
-				isSistory: true,
-				// 搜索内容
-				searchcontent: "",
-				// 判断
-				loadModal: false,
-				// 题型
-				dataa: [],
-				// 答案
-				optionList: ['A', 'B', 'C', 'D'],
-				// 搜索历史
-				historyList: []
-			}
-		},
-		methods: {
-			// 搜索历史搜索
-			history(text) {
-				console.log(text)
-				uni.request({
-					url: `${baseUrl}/serach`,
-					method: "POST",
-					data: {
-						"text": text
-					},
-					success: (res) => {
-						this.searchcontent = ""
-						if (res.data.data.length > 0) {
-							this.dataa = res.data.data.map(item => {
-								item.options = JSON.parse(item.options)
-								return item
-							})
-							this.isSistory = false
-						} else {
-							uni.showModal({
-								title: '提示',
-								content: '没有找到符合此条件的内容',
-								success: function(res) {
-									if (res.confirm) {
-										this.isSistory = true
-									} else if (res.cancel) {
-										this.isSistory = true
-									}
-								}
-							});
-						}
+import { baseUrl } from "@/api/index.js";
+export default {
+  data() {
+    return {
+      // 搜索历史是否显示
+      isSistory: true,
+      // 搜索内容
+      searchcontent: "",
+      // 判断
+      loadModal: false,
+      // 题型
+      dataa: [],
+      // 答案
+      optionList: ["A", "B", "C", "D"],
+      // 搜索历史
+      historyList: [],
+    };
+  },
+  methods: {
+    // 搜索历史搜索
+    history(text) {
+      console.log(text);
+      uni.request({
+        url: `${baseUrl}/serach`,
+        method: "POST",
+        data: {
+          text: text,
+        },
+        success: (res) => {
+          this.searchcontent = "";
+          if (res.data.data.length > 0) {
+            this.dataa = res.data.data.map((item) => {
+              item.options = JSON.parse(item.options);
+              return item;
+            });
+            this.isSistory = false;
+          } else {
+            uni.showModal({
+              title: "提示",
+              content: "没有找到符合此条件的内容",
+              success: function (res) {
+                if (res.confirm) {
+                  this.isSistory = true;
+                } else if (res.cancel) {
+                  this.isSistory = true;
+                }
+              },
+            });
+          }
 
-						this.loadModal = false
-					}
-				});
-			},
-			// 清除历史
-			empty() {
-				this.historyList.length = 0
-				this.historyList.splice(0, this.historyList.length);
-				uni.setStorageSync('lishi', []);
-			},
-			// 搜索接口
-			search: function(text) {
-				if (this.searchcontent.length >= 1) {
-					// 添加
-					this.historyList.push(this.searchcontent)
-					// 数组去重
-					this.historyList=Array.from(new Set(this.historyList))
-					uni.setStorageSync('lishi', this.historyList);
-					this.loadModal = true
-					uni.request({
-						url: `${baseUrl}/serach`,
-						method: "POST",
-						data: {
-							"text": this.searchcontent
-						},
-						success: (res) => {
-							this.searchcontent = ""
-							if (res.data.data.length > 0) {
-								this.dataa = res.data.data.map(item => {
-									item.options = JSON.parse(item.options)
-									return item
-								})
-								this.isSistory = false
-							} else {
-								uni.showModal({
-									title: '提示',
-									content: '没有找到符合此条件的内容',
-									success: function(res) {
-										if (res.confirm) {
-											this.isSistory = true
-										} else if (res.cancel) {
-											this.isSistory = true
-										}
-									}
-								});
-							}
+          this.loadModal = false;
+        },
+      });
+    },
+    // 清除历史
+    empty() {
+      this.historyList.length = 0;
+      this.historyList.splice(0, this.historyList.length);
+      uni.setStorageSync("lishi", []);
+    },
+    // 搜索接口
+    search: function (text) {
+      if (this.searchcontent.length >= 1) {
+        // 添加
+        this.historyList.push(this.searchcontent);
+        // 数组去重
+        this.historyList = Array.from(new Set(this.historyList));
+        uni.setStorageSync("lishi", this.historyList);
+        this.loadModal = true;
+        uni.request({
+          url: `${baseUrl}/serach`,
+          method: "POST",
+          data: {
+            text: this.searchcontent,
+          },
+          success: (res) => {
+            this.searchcontent = "";
+            if (res.data.data.length > 0) {
+              this.dataa = res.data.data.map((item) => {
+                item.options = JSON.parse(item.options);
+                return item;
+              });
+              this.isSistory = false;
+            } else {
+              uni.showModal({
+                title: "提示",
+                content: "没有找到符合此条件的内容",
+                success: function (res) {
+                  if (res.confirm) {
+                    this.isSistory = true;
+                  } else if (res.cancel) {
+                    this.isSistory = true;
+                  }
+                },
+              });
+            }
 
-							this.loadModal = false
-						}
-					});
-				} else {
-					uni.showModal({
-						title: '提示',
-						content: '搜索框内容不能为空',
-						success: function(res) {
-							if (res.confirm) {} else if (res.cancel) {}
-						}
-					});
-				}
-
-			},
-			todetails(id) {
-				const searchcontent = this.searchcontent
-				uni.navigateTo({
-					url: `/pages/search/details?id=` + id
-				})
-			},
-		},
-		onLoad: function() {
-			this.historyList = uni.getStorageSync("lishi");
-		}
-
-	}
+            this.loadModal = false;
+          },
+        });
+      } else {
+        uni.showModal({
+          title: "提示",
+          content: "搜索框内容不能为空",
+          success: function (res) {
+            if (res.confirm) {
+            } else if (res.cancel) {
+            }
+          },
+        });
+      }
+    },
+    todetails(id) {
+      const searchcontent = this.searchcontent;
+      uni.navigateTo({
+        url: `/pages/search/details?id=` + id,
+      });
+    },
+  },
+  onLoad: function () {
+    this.historyList = uni.getStorageSync("lishi");
+  },
+};
 </script>
 
 <style>
-	.jincheng-input {
-		width: 85%;
-		height: 40px;
-		background: #FAFAFA;
-		border-bottom-left-radius: 18px;
-		border-top-left-radius: 18px;
-		border-top-right-radius: 18px;
-		border-bottom-right-radius: 18px;
-		padding-left: 20px;
-		margin-right: 25px;
-		border: 1px solid red;
+.jincheng-input {
+  width: 85%;
+  height: 40px;
+  background: #fafafa;
+  border-bottom-left-radius: 18px;
+  border-top-left-radius: 18px;
+  border-top-right-radius: 18px;
+  border-bottom-right-radius: 18px;
+  padding-left: 20px;
+  margin-right: 25px;
+  border: 1px solid red;
+}
 
-	}
+.jincheng-search {
+  width: 80px;
+  height: 38px;
+  font-size: 18px;
+  margin-right: 10px;
+  font-weight: bolder;
+}
 
-	.jincheng-search {
-		width: 80px;
-		height: 38px;
-		font-size: 18px;
-		margin-right: 10px;
-		font-weight: bolder;
-	}
+.jincehng-xuan {
+  width: 90%;
+  margin: 10px auto;
+  background: white;
+  border-radius: 10px;
+  word-wrap: break-word;
+  word-break: normal;
+}
 
-	.jincehng-xuan {
-		width: 90%;
-		margin: 10px auto;
-		background: white;
-		border-radius: 10px;
-		word-wrap: break-word;
-		word-break: normal;
-	}
+.jincehng-input-box {
+  display: flex;
+  margin-top: 10px;
+  margin: 25px auto;
+  width: 95%;
+}
 
-	.jincehng-input-box {
-		display: flex;
-		margin-top: 10px;
-		margin: 25px auto;
-		width: 95%;
+.jincehng-ti {
+  padding: 10px;
+  font-size: 18px;
+  letter-spacing: 3px;
+  letter-spacing: 0px;
+}
 
-	}
+.jincheng-font {
+  font-weight: bolder;
+}
 
-	.jincehng-ti {
-		padding: 10px;
-		font-size: 18px;
-		letter-spacing: 3px;
-		letter-spacing: 0px;
+.jincehng-jiexi {
+  padding: 10px;
+}
 
-	}
-
-	.jincheng-font {
-		font-weight: bolder;
-	}
-
-	.jincehng-jiexi {
-		padding: 10px;
-	}
-
-	.jincheng-flex {
-		display: flex;
-	}
+.jincheng-flex {
+  display: flex;
+}
 </style>
